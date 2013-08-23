25d50998533eb30168da4f77a5e1670f5847b4cc
Martin Nicolas Santos <mnsantos@ws6.labo4>
2013-08-23 20:33:09 -0300

    ej3 con cosas

--- ej3/include/problema.h
+++ ej3/include/problema.h
@@ -13,9 +13,19 @@ using namespace std;
 struct Problema{
 
 	vector< vector<int> > _matriz;
+	vector< pair<int,int> > _casilleros;
+	vector< pair<int,int> > _casillerosImportantes;
+	vector< vector<int> > _matrizRes;
+	
 	void resolver();
 	Problema (istream&);
+	void insertarSensor(int);
+	bool leApuntaUnLaserHorizontal(pair<int,int>);
+	bool leApuntaUnLaserVertical(pair<int,int>);
+	void mostrarCasilleros(ostream&);
+	void cargarCasilleros();
 	void mostrarMatriz(ostream&);
+	bool leApuntanDosLasers(pair<int,int>);
 };
 
 #endif
--- ej3/src/ej3.cpp
+++ ej3/src/ej3.cpp
@@ -22,6 +22,63 @@ void Problema::mostrarMatriz(ostream& os){
 	}
 }
 
+void Problema::mostrarCasilleros(ostream& os){
+	cout<<"Casilleros"<<endl;
+	for (int i=0;i<_casilleros.size();++i){
+		os << "(" << _casilleros[i].first+1 << " " << _casilleros[i].second+1 << ") ";
+	}
+	cout<<endl;
+	cout<<"Casilleros importantes"<<endl;
+	for (int i=0;i<_casillerosImportantes.size();++i){
+		os << "(" << _casillerosImportantes[i].first+1 << " " << _casillerosImportantes[i].second+1 << ") ";
+	}
+}
+
+void Problema::cargarCasilleros(){
+	for (int i=0;i<_matriz.size();++i){
+		for (int j=0;j<_matriz[i].size();++j){
+			if (_matriz[i][j] == 1) _casilleros.push_back(make_pair(i,j));
+			if (_matriz[i][j] == 2) _casillerosImportantes.push_back(make_pair(i,j));
+		}
+	}
+}
+
+bool Problema::leApuntaUnLaserHorizontal(pair<int,int> casillero){
+	for (int j=casillero.second; j<_matriz[casillero.first].size();++j){
+		if (_matriz[casillero.first][j] == 3 || _matriz[casillero.first][j] == 4) return true;
+		if (_matriz[casillero.first][j] == 0){
+			for (int j=casillero.second; j>=0;--j){
+				if (_matriz[casillero.first][j] == 3 || _matriz[casillero.first][j] == 4) return true;
+				if (_matriz[casillero.first][j] == 0) return false;
+			}
+			return false;
+		}
+	}
+}
+
+bool Problema::leApuntaUnLaserVertical(pair<int,int> casillero){
+	for (int i=casillero.first; i<_matriz[i].size();++i){
+		if (_matriz[i][casillero.second] == 5 || _matriz[i][casillero.second] == 4) return true;
+		if (_matriz[i][casillero.second] == 0){
+			for (int i=casillero.first; i>=0;--i){
+				if (_matriz[i][casillero.second] == 5 || _matriz[i][casillero.second] == 4) return true;
+				if (_matriz[i][casillero.second] == 0) return false;
+			}
+			return false;
+		}
+	}
+}
+
+bool Problema::leApuntanDosLasers(pair<int,int> casillero){
+	if (leApuntaUnLaserVertical(casillero) && leApuntaUnLaserHorizontal(casillero)) return true;
+	else return false;
+}
+
 void Problema::resolver(){
 	mostrarMatriz(cout);
+	cargarCasilleros();
+	mostrarCasilleros(cout);
+	_matriz[1][2] = 5;
+	cout<<endl;
+	cout<<leApuntaUnLaserVertical(make_pair(2,2))<<endl;
 }
